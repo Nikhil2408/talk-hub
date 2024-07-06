962466f1ab7e8d593cce30fb6a94bc12df1803e6
Nikhil Bansal <nikbansal1998@gmail.com>
2024-07-06 21:05:19 +0530

    fix: display auth user name when no chat is selected

--- frontend/src/components/ChatContainer/NoSelectedConversation.jsx
+++ frontend/src/components/ChatContainer/NoSelectedConversation.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthContext } from "../../../context/AuthContext";
 
 
 const NoSelectedConversation = () => {
-  return (
-    <div className="flex flex-col justify-center items-center flex-1">
-        <h2>Welcome Nikhil Bansal</h2>
-        <h3>Select a chat to start messaging</h3>
-    </div>
-  )
+
+    const {authUser} = useAuthContext();
+
+    return (
+        <div className="flex flex-col justify-center items-center flex-1">
+            <h2>Welcome {authUser.fullName}</h2>
+            <h3>Select a chat to start messaging</h3>
+        </div>
+    )
 }
 
 export default NoSelectedConversation
